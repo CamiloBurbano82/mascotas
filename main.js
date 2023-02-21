ff280b6ba78f9102dcac90905d3b7430d2a08e99
Camilo Burbano <camiloburbano82@gmail.com>
2023-02-21 17:55:17 -0500

    ajustes finales

--- main.js
+++ main.js
@@ -13,16 +13,27 @@ createApp({
             alert: false,
             message: '',
             pets: [
-                { id: 1, name: 'max', race: 'labrador', color: 'blanco', specie: 'Perro', age: '45', gender: 'Macho', img: 'images/dog1.jpg', description: 'muy lindo', status: true },
-                { id: 2, name: 'pacho', race: 'nn', color: 'blancho y negro', specie: 'Gato', age: '15', gender: 'Macho', img: 'images/cat1.png', description: 'muy dormilon', status: false },
-                { id: 3, name: 'condor', race: 'bulldog', color: 'blanco y naranja', specie: 'Perro', age: '82', gender: 'Macho', img: 'images/dog4.jpg', description: 'muy jugueton', status: false },
-                { id: 4, name: 'orion', race: 'nn', color: 'dorado', specie: 'Gato', age: '20', gender: 'Macho', img: 'images/cat4.jpg', description: 'muy travieso', status: false }
+                {
+                    id: 1, name: 'max', race: 'labrador', color: 'blanco', specie: 'Perro', age: '45',
+                    gender: 'Macho', img: 'images/dog1.jpg', description: 'muy lindo', status: true
+                },
+                {
+                    id: 2, name: 'pacho', race: 'Bombay', color: 'colorado', specie: 'Gato', age: '1',
+                    gender: 'Macho', img: 'images/cat1.png', description: 'muy dormilon', status: false
+                },
+                {
+                    id: 3, name: 'condor', race: 'bulldog', color: 'blanco', specie: 'Perro', age: '82',
+                    gender: 'Macho', img: 'images/dog4.jpg', description: 'muy jugueton con los niños y muy tierno con los mayores', status: false
+                },
+                {
+                    id: 4, name: 'orion', race: 'Bengalí', color: 'dorado', specie: 'Gato', age: '20',
+                    gender: 'Macho', img: 'images/cat4.jpg', description: 'muy travieso', status: false
+                }
             ],
             images: [
                 { specie: 'Perro', img: 'images/dog1.jpg' }, { specie: 'Perro', img: 'images/dog2.jfif' }, { specie: 'Perro', img: 'images/dog3.jpg' }, { specie: 'Perro', img: 'images/dog4.jpg' },
                 { specie: 'Gato', img: 'images/cat1.png' }, { specie: 'Gato', img: 'images/cat2.webp' }, { specie: 'Gato', img: 'images/cat3.jpg' }, { specie: 'Gato', img: 'images/cat4.jpg' }
             ],
-
             name: '',
             race: '',
             color: '',
@@ -33,7 +44,9 @@ createApp({
             description: '',
             auxSelect: '',
             imageSelect: '',
-
+            isAdopt: true,
+            isForAdoption: false,
+            isviewPets: false
         }
     },
     methods: {
@@ -61,72 +74,98 @@ createApp({
             this.users[0].type = 1;
             this.users[2].type = 1;
             this.users[4].type = 1;
-            console.log(this.users);
+            console.log('users', this.users);
 
         },
 
         loadPets() {
             if (localStorage.getItem('pets') == null ||
-                localStorage.getItem('ets') == undefined) {
+                localStorage.getItem('pets') == undefined) {
                 localStorage.setItem('pets', JSON.stringify(this.pets))
             } else {
                 localStorage.setItem('pets', localStorage.getItem('pets'))
                 this.pets = JSON.parse(localStorage.getItem('pets'));
             }
-            console.log(this.pets);
+            console.log('pets', this.pets);
         },
 
         login() {
-
             if (this.username != '' && this.password != '') {
-                // this.currentUser = this.users.filter( user => user.password == this.password && user.username == this.username);
-
                 this.users.forEach(user => {
                     if (user.password == this.password && user.username == this.username) {
                         this.currentUser = user;
                     }
                 });
-
-                console.log(this.currentUser.name);
-                console.log(this.currentUser.picture);
-                console.log(this.currentUser.cell);
-                console.log(this.currentUser.email);
-                if (this.currentUser) {
+                if (this.currentUser?.password == this.password && this.currentUser?.username == this.username) {
                     if (this.currentUser.type == 1) {
                         this.userAdmin = true;
                     }
                     this.loginUser = false;
                     this.isUser = true;
+                    this.username = '';
+                    this.password = '';
                 } else {
-                    this.message = 'Username o contraseña incorrecto'
+                    this.message = 'Usuario o contraseña incorrecto';
+                    this.alert = true;
                     console.log(this.message);
                 }
             } else {
-                this.message = 'Todos los campos son obligatorios'
-                console.log(this.message);
+                this.message = 'Todos los campos son obligatorios';
+                this.alert = true;
             }
-
         },
 
         logout() {
             this.currentUser = '';
             this.isUser = false;
             this.userAdmin = false;
-            this.user = true;
+            this.loginUser = true;
+            this.alert = false;
+        },
+
+        pages(page) {
+            if (page == 1) {
+                this.isAdopt = true;
+                this.isForAdoption = false;
+                this.isviewPets = false;
+            }
+            else if (page == 2) {
+                this.isAdopt = false;
+                this.isForAdoption = true;
+                this.isviewPets = false;
+            }
+            else {
+                this.isAdopt = false;
+                this.isForAdoption = false;
+                this.isviewPets = true;
+            }
         },
 
         closeAlert() {
-
+            this.alert = false;
         },
 
         adopt(id) {
-
             this.pets.forEach(pet => {
                 if (pet.id == id) pet.status = true;
             })
             localStorage.setItem('pets', JSON.stringify(this.pets));
             this.pets = JSON.parse(localStorage.getItem('pets'));
 
+            Swal.fire({
+                title: `Has adoptado a un amigo!!!`,
+                text: '¿Que tal si adoptas otro amigo?',
+                width: 600,
+                padding: '3em',
+                color: 'white',
+                background: '#272B33',
+
+                backdrop: `
+                  rgba(0,176,200,0.4)
+                  left top
+                  no-repeat
+                `
+            })
         },
 
         selectPerfil(img) {
@@ -145,17 +184,11 @@ createApp({
                     this.auxSelect = button;
                 }
             }
-
-            console.log(button);
         },
-        // selectImagePerfil() {
-        //     console.log('Seleccionado', this.imageSelect);
-        // },
 
         forAdoption() {
-
             if (this.name != '' && this.race != '' && this.color != '' && this.specie != ''
-                && this.age != '' && this.gender != '' && this.imageSelect != '' && this.description != '') {
+                && this.age != '' && this.age > 0 && this.gender != '' && this.imageSelect != '' && this.description != '') {
                 let id = this.pets[this.pets.length - 1].id + 1;
 
                 this.pets.push({
@@ -169,31 +202,49 @@ createApp({
                     img: this.imageSelect.img,
                     description: this.description,
                     status: false
+                });
+                localStorage.setItem('pets', JSON.stringify(this.pets));
+                this.pets = JSON.parse(localStorage.getItem('pets'));
+                Swal.fire({
+                    title: `Mascota dada en adopción`,
+                    width: 600,
+                    padding: '3em',
+                    color: 'white',
+                    background: '#272B33',
+
+                    backdrop: `
+                      rgba(0,176,200,0.4)
+                      left top
+                      no-repeat
+                    `
                 })
-                this.loadPets();
-                console.log(this.pets);
-            }else{
+
+                this.name = '';
+                this.race = '';
+                this.color = '';
+                this.specie = '';
+                this.age = '';
+                this.gender = '';
+                this.imageSelect = '';
+                this.description = '';
+
+            } else {
                 console.log('Todos los campos son obligatorios');
+                Swal.fire({
+                    title: `Todos los campos son obligatorios`,
+                    width: 600,
+                    padding: '3em',
+                    color: 'white',
+                    background: '#272B33',
+
+                    backdrop: `
+                      rgba(0,176,200,0.4)
+                      left top
+                      no-repeat
+                    `
+                })
             }
-
-
-
         },
-        prueba() {
-            this.currentUser = {
-                cell: "(672) 823 4924",
-                email: "zacarias.griego@example.com",
-                name: "Zacarías Griego",
-                password: "spoons",
-                picture: "https://randomuser.me/api/portraits/thumb/men/8.jpg",
-                type: 1,
-                username: "organiccat787"
-            };
-        }
-
-
-
-
     },
     mounted() {
 
